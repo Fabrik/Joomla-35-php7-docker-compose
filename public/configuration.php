c5a0aee830adb4f173e2e5411cda2b00ebd45b50
pollen8 <rob@pollen-8.co.uk>
2016-04-28 19:57:15 +0200

    update j351 files, trying to work out correct tmp/log paths

--- public/configuration.php
+++ public/configuration.php
@@ -56,8 +56,8 @@ class JConfig {
 	public $unicodeslugs = '0';
 	public $feed_limit = '10';
 	public $feed_email = 'none';
-	public $log_path = 'C:\\xampp\\htdocs\\joomla35/logs';
-	public $tmp_path = 'C:\\xampp\\htdocs\\joomla35/tmp';
+	public $log_path = '/var/www/html/logs';
+	public $tmp_path = '/var/www/html/tmp';
 	public $lifetime = '15';
 	public $session_handler = 'database';
 }
